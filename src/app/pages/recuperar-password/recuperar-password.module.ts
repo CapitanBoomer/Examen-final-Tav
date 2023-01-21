21a840d1526880f7fe03ec80f034787d5aa7a5c8
JhoelMaldonado <jh.maldonado@duocuc.cl>
2023-01-21 11:34:02 -0300

    "arreglos appmodules"

--- src/app/pages/recuperar-password/recuperar-password.module.ts
+++ src/app/pages/recuperar-password/recuperar-password.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { IonicModule } from '@ionic/angular';
 
 import { RecuperarPasswordPageRoutingModule } from './recuperar-password-routing.module';
 
 import { RecuperarPasswordPage } from './recuperar-password.page';
+
+// desarrollladores
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   imports: [
