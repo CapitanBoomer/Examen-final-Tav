917938c962721803ec91191f1e323ed0a783abc0
JhoelMaldonado <jh.maldonado@duocuc.cl>
2023-01-23 17:59:19 -0300

    "errores"

--- src/app/interfaces/usarios/usuarios.ts
+++ src/app/interfaces/usarios/usuarios.ts
@@ -14,13 +14,13 @@ export interface Datoscompletos{
   lastName: string,
   email:string
 }
-<<<<<<< HEAD
+
 export interface ObtCon{
   username : string,
   email : string
-=======
+}
+
 
 export interface Carrera extends Datoscompletos{
   nombreCarrera : string
->>>>>>> 96bd7dd17e837686fc8b42df7d2c0d7e1e8b77b5
 }
